e521e408d2399874138c8bbd66d72012a0b7337a
agorapilled <johngardner13@outlook.com>
2026-05-30 03:21:41 -0400

    v0

--- app/components/LeagueTabs.tsx
+++ app/components/LeagueTabs.tsx
@@ -42,7 +42,7 @@ export default function LeagueTabs({
                   damping: 34,
                   mass: 0.8,
                 }}
-                className="absolute inset-0 hidden rounded-md bg-zinc-900 sm:block"
+                className="absolute inset-0 hidden rounded-lg bg-zinc-900 sm:block"
               />
             ) : null}
 
